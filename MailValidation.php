8c70e232645e32d5c443d316b86a3029d985c6e5
Joram Knaack <joram@lonet.org>
2013-12-17 20:52:26 +0100

    added more comments

--- MailValidation.php
+++ MailValidation.php
@@ -3,18 +3,30 @@
 /*
  * php-mail-validation
  * 
+ * $Id: MailValidation.php 4 2013-12-17 20:35:08Z joramk $
+ * 
+ * @summary   Validate an E-Mail address against various checks.
+ * @tutorial  $validated = new MailValidation()
+ *                       ->setEmailAddress('joe@example.org')
+ *                       ->validate();
+ * @author    Joram Knaack <joramk@gmail.com>
+ * @copyright (C) Copyright 2013 Joram Knaack
+ * @license   http://www.gnu.org/licenses/gpl.html
+ * @link      http://code.google.com/p/php-mail-validation/
+ * 
  */
 
 class MailValidation {
 	
 	private $EmailAddress;
+	
 	private $DoCheckLength = true;
 	private $DoCheckTopLevelDomain = true;
 	private $DoCheckControlChars = true;
 	private $DoCheckDNS = true;
 	private $DoAllowIpAsDomainPart = true;
 	
-	static private $MailboxDomainSeperator = '@';
+	static private $MailboxDomainSeparator = '@';
 	static private $MinimumDomainLength = 3;
 	static private $MaximumDomainLength = 255;
 	static private $MinimumMailboxNameLength = 1;
@@ -22,26 +34,45 @@ class MailValidation {
 	static private $MaximumOverallLength = 256; // RFC 2821
 	static private $TopLevelDomainFile = 'MailValidation.tld';
 	static private $TopLevelDomainRegEx = '/\.([A-Za-z0-9-])$/';
-	static private $ControlCharsRegex = '/[\x00-\x1F\x7F-\xFF]/';
+	static private $ControlCharsRegEx = '/[\x00-\x1F\x7F-\xFF]/';
+	static private $ValidateIp4RegEx = '/^((2[0-4]|1\d|[1-9])?\d|25[0-5])(\.(?1)){3}\z/';
+	static private $ValidateIp6RegEx = '/^(((?=(?>.*?(::))(?!.+\3)))\3?|([\dA-F]{1,4}(\3|:(?!$)|$)|\2))(?4){5}((?4){2}|((2[0-4]|1\d|[1-9])?\d|25[0-5])(\.(?7)){3})\z/i';
 	
+	/**
+	 * Constructor to accept an E-Mail address for validation
+	 * 
+	 * @param string $emailAddress Set the E-Mail address to validate.
+	 */
 	public function __construct($emailAddress = null) {
 		if(!empty($emailAddress))
 			$this->EmailAddress = $emailAddress;
 	}
 	
+	/**
+	 * Validate the given E-Mail address
+	 * 
+	 * @return boolean True when the given E-Mail address is valid, otherwise false.
+	 */
 	public function validate() {
 		if(empty($this->EmailAddress))
 			return false;
-		if(strstr($this->EmailAddress, '@' === false))
+		if(strstr($this->EmailAddress, self::$MailboxDomainSeparator) === false)
 			return false;
 		$addressParts = $this->_splitAddressParts($this->EmailAddress);
+		$domainPart = $this->_convertUtf8ToIdnDomain($addressParts['domain']);
 		return $this->CheckControlChars($this->EmailAddress)
 				&& $this->CheckLength($this->EmailAddress)
-				&& ($this->CheckIpAddress($addressParts['domain'])
-						|| $this->CheckTopLevelDomain($addressParts['domain']))
+				&& ($this->CheckIpAddress($domainPart)
+						|| $this->CheckTopLevelDomain($domainPart))
 				;
 	} 
 	
+	/**
+	 * Sets the E-Mail address to validate.
+	 * 
+	 * @param string $value E-Mail address
+	 * @return \MailValidation
+	 */
 	public function setEmailAddress($value) {
 		if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc())
 			$this->EmailAddress = stripslashes($value);
@@ -50,6 +81,12 @@ class MailValidation {
 		return $this;
 	}
 	
+	/**
+	 * Activates or deacitvates all checks
+	 * 
+	 * @param boolean $value True to activate all checks, false to disable them
+	 * @return \MailValidation
+	 */
 	public function setAllChecks($value = true) {
 		$this->setCheckDNS($value);
 		$this->setCheckControlChars($value);
@@ -58,32 +95,80 @@ class MailValidation {
 		return $this;
 	}
 
+	/**
+	 * Activates or deactivates the DNS record check for the domain part
+	 * of an E-Mail address.
+	 * 
+	 * @param boolean $value True to activate the check, false to disable it
+	 * @return \MailValidation
+	 */
 	public function setCheckDNS($value = true) {
 		$this->DoCheckDNS = $value === true ? true : false;
 		return $this;
 	}
 	
+	/**
+	 * Activates or deactivates the check for special control chars
+	 * which may be malicious included in the E-Mail address.
+	 * 
+	 * @param boolean $value True to activate the check, false to disable it
+	 * @return \MailValidation
+	 */
 	public function setCheckControlChars($value = true) {
 		$this->DoCheckControlChars = $value === true ? true : false;
 		return $this;
 	}
 	
+	/**
+	 * Activates or deactivates the check for the length of the
+	 * E-Mail address.
+	 * 
+	 * @param boolean $value True to activate the check, false to disable it
+	 * @return \MailValidation
+	 */
 	public function setCheckLength($value = true) {
 		$this->DoCheckLength = $value === true ? true : false;
 		return $this;
 	}
 	
+	/**
+	 * Activates or deactivates the check for valid top level domain
+	 * in the domain part of an E-Mail address.
+	 * 
+	 * @param type $value
+	 * @return \MailValidation
+	 */
 	public function setCheckTopLevelDomain($value = true) {
 		$this->DoCheckTopLevelDomain = $value === true ? true : false;
 		return $this;
 	}
 	
+	/**
+	 * Check the given E-Mail address against malicious control characters
+	 * 
+	 * @param string $emailAddress E-Mail address
+	 * @return boolean True if control chars are detected
+	 */
 	private function CheckControlChars($emailAddress) {
 		return $this->DoCheckControlChars || $this->_checkControlChars($emailAddress);
 	}
 	
+	/**
+	 * 
+	 * @param type $emailAddress
+	 * @return type
+	 */
 	private function _checkControlChars($emailAddress) {
-		return preg_match(self::$ControlCharsRegex, $emailAddress);
+		return preg_match(self::$ControlCharsRegEx, $emailAddress);
+	}
+	
+	/**
+	 * 
+	 * @param type $domainPart
+	 * @return type
+	 */
+	private function _convertUtf8ToIdnDomain($domainPart) {
+		return @idn_to_ascii($domainPart);
 	}
 	
 	/**
@@ -117,6 +202,7 @@ class MailValidation {
 	
 	/**
 	 * Checks if the TLD of the domain part is well-known (internet routable)
+	 * against a text file taken from IANA which includes all valid TLDs.
 	 * 
 	 * http://data.iana.org/TLD/tlds-alpha-by-domain.txt
 	 * 
@@ -155,7 +241,7 @@ class MailValidation {
 	 * @return array An associative array consisting of the 'mailbox' and 'domain' part.
 	 */
 	private function _splitAddressParts($emailAddress) {
-		$seperatorPosition = strrpos($emailAddress, self::$MailboxDomainSeperator);
+		$seperatorPosition = strrpos($emailAddress, self::$MailboxDomainSeparator);
 		return array('mailbox' => substr($emailAddress, 0, $seperatorPosition),
 				'domain' => substr($emailAddress, $seperatorPosition + 1));
 	}
@@ -179,12 +265,19 @@ class MailValidation {
 	
 	/**
 	 * Validates an IPv4 address utilising PHP's filter_var function
+	 * or uses preg_matc as fallback if function is missing or the
+	 * validate_ip filter is not available.
 	 * 
 	 * @param string $ipAddress
 	 * @return boolean True if the supplied address is a valid IPv4 address
 	 */
 	private function _validateIp4Address($ipAddress) {
-		return filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+		if (function_exists('filter_var')
+				&& in_array('validate_ip', filter_list())) {
+			return filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+		} else {
+			return preg_match(self::$ValidateIp4RegEx, $ipAddress);
+		}
 	}
 	
 	/**
@@ -194,6 +287,11 @@ class MailValidation {
 	 * @return boolean True if the supplied address is a valid IPv6 address
 	 */
 	private function _validateIp6Address($ipAddress) {
-		return filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+		if(function_exists('filter_var')
+				&& in_array('validate_ip', filter_list())) {
+			return filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+		} else {
+			return preg_match(self::$ValidateIp6RegEx, $ipAddress);
+		} 
 	}
 }
